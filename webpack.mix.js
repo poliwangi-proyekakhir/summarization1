7edfaa5077e8ae35cdff361bb4f7649e00efa08e
kamaliaervinadewi <kamaliaervinatipoliwangi42@gmail.com>
2020-04-10 22:15:15 +0700

    my first commit laravel 5.7

---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css');
+
+.styles([
+   		'resources/css/libs/blog-spot.css',
+   		'resources/css/libs/bootstrap.css',
+   		'resources/css/libs/font-awesome.css',
+   		'resources/css/libs/metisMenu.css',
+   		'resources/css/libs/sb-admin-2.css'
+   	], './public/css/libs.css')
+
+   .scripts([
+   		'resources/js/js/libs/jquery.js',
+   		'resources/js/js/libs/bootstrap.js',
+   		'resources/js/js/libs/metisMenu.js',
+   		'resources/js/js/libs/sb-admin-2.js'
+   		'resources/js/js/libs/scripts.js'
+   		], './public/js/libs.js');
